ded4c53b609df48ed7e1fb151db5a0075f68b95f
Andries Bester <andriesbester@Andriess-MacBook-Air.local>
2024-02-03 12:48:32 +0200

    updated partial naming

--- page-home.php
+++ page-home.php
@@ -14,36 +14,44 @@
 
 get_header();
 ?>
+	<div class="content-container">
+		<div class="hero">Hero Section</div>
+		<div class="logo-reel">Logo reel Section</div>
+		<div class="services">Services section</div>
+		<div class="featured">Featured Section</div>
+		<div class="blog">Blog Section</div>
 
-	<div class="hero">Hero Section</div>
-	<div class="logo-reel">Logo reel Section</div>
-	<div class="services">Services section</div>
-	<div class="featured">Featured Section</div>
-	<div class="blog">Blog Section</div>
 
 
-
-	<div class="team">
-		<div class="left">
-			<div class="content">
-				<h2>About the Team</h2>
-				<p>Discover the heart of Two Chefs Catering: a blend of founders Herman Hanekom and Andre Schoeman's ambitious vision with a team of passionate, skilled professionals. Together, we ensure every detail of your event is perfected, reflecting our commitment to innovation, excellence, and exceptional catering.</p>
-				<a href="#" class="button button-primary">Contact</a>
+		<div class="team">
+			<div class="left">
+				<div class="content">
+					<h2>About the Team</h2>
+					<p>Discover the heart of Two Chefs Catering: a blend of founders Herman Hanekom and Andre Schoeman's ambitious vision with a team of passionate, skilled professionals. Together, we ensure every detail of your event is perfected, reflecting our commitment to innovation, excellence, and exceptional catering.</p>
+					<a href="#" class="button button-primary">Contact</a>
+				</div>
+			</div>
+			<div class="right">
+				<img src="https://easydrawingguides.com/wp-content/uploads/2017/04/how-to-draw-goku-featured-image-1200.png" alt="" class="team-image">
 			</div>
 		</div>
-		<div class="right">
-			<img src="https://easydrawingguides.com/wp-content/uploads/2017/04/how-to-draw-goku-featured-image-1200.png" alt="" class="team-image">
-		</div>
+		<h1>this is an h1</h1>
+		<h2>this is an h2</h2>
+		<h3>this is an h3</h3>
+		<h4>this is an h4</h4>
+		<h5>this is an h5</h5>
+		<h6>this is an h6</h6>
+
+
+
+
+
+
+		<div class="testimonial">testimonial section</div>
+		<div class="faq">FAQs</div>
+		<div class="callout">Callout</div>
 	</div>
 
-
-
-
-
-
-	<div class="testimonial">testimonial section</div>
-	<div class="faq">FAQs</div>
-	<div class="callout">Callout</div>
 
 
 <?php
