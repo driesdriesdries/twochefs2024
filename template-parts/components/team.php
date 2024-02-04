e1746d6a4a2eb30d6685f558d21eec70aafda5fd
Andries Bester <andriesbester@Andriess-MacBook-Air.local>
2024-02-04 13:07:15 +0200

    rebuilt the team section

--- template-parts/components/team.php
+++ template-parts/components/team.php
@@ -1,12 +1,12 @@
 <div class="team">
 	<div class="left">
 		<div class="content">
-			<h2>About the Team</h2>
+			<h2>About the team</h2>
 			<p>Discover the heart of Two Chefs Catering: a blend of founders Herman Hanekom and Andre Schoeman's ambitious vision with a team of passionate, skilled professionals. Together, we ensure every detail of your event is perfected, reflecting our commitment to innovation, excellence, and exceptional catering.</p>
-			<a href="#" class="button button-primary">Contact</a>
 		</div>
 	</div>
 	<div class="right">
-		<img src="https://easydrawingguides.com/wp-content/uploads/2017/04/how-to-draw-goku-featured-image-1200.png" alt="" class="team-image">
+		<div class="image">
+		</div>
 	</div>
 </div>
