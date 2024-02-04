b787b9c908c45522d1f2448bb956aeb5b8a8e569
Andries Bester <andriesbester@Andriess-MacBook-Air.local>
2024-02-04 14:17:53 +0200

    callout and featured on work

--- template-parts/components/callout.php
+++ template-parts/components/callout.php
@@ -1,1 +1,6 @@
-<div class="callout">callout</div>
+<div class="callout">
+    <div class="content">
+        <h2>Make Your Event Deliciously Memorable!</h2>
+        <a href="#" class="button button-primary">Contact</a>
+    </div>
+</div>
--- template-parts/components/featured.php
+++ template-parts/components/featured.php
@@ -1,1 +1,15 @@
-<div class="featured">Featured</div>
+<div class="featured">
+    <div class="left">
+        <div class="content">
+            <h2>As Featured On</h2>
+            <p>Our culinary artistry has been recognized and featured in notable publications and platforms. Explore our celebrated journey in the catering world!</p>
+        </div>
+    </div>
+    <div class="right">
+        <div class="logo-box">
+            <div class="logo logo-1">1</div>
+            <div class="logo logo-2">2</div>
+            <div class="logo logo-3">3</div>
+        </div>
+    </div>
+</div>
