5039dd2aed89e11d08686c0b9c84f51d9e19dd75
Andries Bester <andriesbester@Andriess-MacBook-Air.local>
2024-02-05 13:14:36 +0200

    services section build

--- template-parts/components/services.php
+++ template-parts/components/services.php
@@ -1,1 +1,44 @@
-<div class="services">services</div>
+<div class="services">
+    <div class="services-heading-block">
+        <h2>Services</h2>
+        <p>Explore our comprehensive range of tailored solutions designed to meet your unique needs</p>
+    </div>
+    <div class="services-card-group">
+        <div class="card" onclick="location.href='https://www.rollingstone.com/';">
+            <div class="card-top">
+                image placeholder
+            </div>
+            <div class="card-bottom">
+                <h5>Corporate Functions</h5>
+                <p>See how we’ve helped other businesses rest easy.</p>
+            </div>
+        </div>
+        <div class="card" onclick="location.href='https://www.rollingstone.com/';">
+            <div class="card-top">
+                image placeholder
+            </div>
+            <div class="card-bottom">
+                <h5>Corporate Functions</h5>
+                <p>See how we’ve helped other businesses rest easy.</p>
+            </div>
+        </div>
+        <div class="card" onclick="location.href='https://www.rollingstone.com/';">
+            <div class="card-top">
+                image placeholder
+            </div>
+            <div class="card-bottom">
+                <h5>Corporate Functions</h5>
+                <p>See how we’ve helped other businesses rest easy.</p>
+            </div>
+        </div>
+        <div class="card" onclick="location.href='https://www.rollingstone.com/';">
+            <div class="card-top">
+                image placeholder
+            </div>
+            <div class="card-bottom">
+                <h5>Corporate Functions</h5>
+                <p>See how we’ve helped other businesses rest easy.</p>
+            </div>
+        </div>
+    </div>
+</div>
