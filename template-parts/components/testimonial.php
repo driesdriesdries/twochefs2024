a94e3763a6121728293f90d0f410c44289c4b0b6
Andries Bester <andriesbester@Andriess-MacBook-Air.local>
2024-02-05 11:36:18 +0200

    created the testimonial section

--- template-parts/components/testimonial.php
+++ template-parts/components/testimonial.php
@@ -1,1 +1,34 @@
-<div class="testimonial">testimonial section</div>
+<div class="testimonial">
+    <div class="testimonial-inner">
+        <div class="section-heading-box">
+            <h2>Our Clients Thought</h2>
+            <p>Real Stories from Satisfied Diners: Our Customers Share Their TwoChefs Experience</p>
+        </div>
+        <div class="testimonial-box">
+            <div class="testimonial-instance">
+                <div class="heading-block">
+                    <div class="portrait"></div>
+                    <div class="description">
+                        <h5>Dries Bester</h5>
+                        <p>Director of product</p>
+                    </div>
+                </div>
+                <div class="content-block">
+                    <p>“Discovering TwoChefs has been a game-changer for my family and me. As a busy parent, finding time to prepare healthy and delicious meals was always a struggle. But with TwoChefs, we've been enjoying a variety of gourmet meals that cater to our diverse tastes.”</p>
+                </div>
+            </div>
+            <div class="testimonial-instance">
+                <div class="heading-block">
+                    <div class="portrait"></div>
+                    <div class="description">
+                        <h5>Dries Bester</h5>
+                        <p>Director of product</p>
+                    </div>
+                </div>
+                <div class="content-block">
+                    <p>“Discovering TwoChefs has been a game-changer for my family and me. As a busy parent, finding time to prepare healthy and delicious meals was always a struggle. But with TwoChefs, we've been enjoying a variety of gourmet meals that cater to our diverse tastes.”</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
