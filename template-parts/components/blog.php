88968042c4dac86a866c26b97386b4d28d143df0
Andries Bester <andriesbester@Andriess-MacBook-Air.local>
2024-02-07 13:53:51 +0200

    created the logo slider and some of the blog section

--- template-parts/components/blog.php
+++ template-parts/components/blog.php
@@ -1,1 +1,13 @@
-<div class="blog">blog</div>
+<div class="blog">
+    <div class="instance-group">
+        <div class="post-instance">
+            <h5>Dining with the artists at nooitgedacght</h3>
+        </div>
+        <div class="post-instance">
+            <h5>Dining with the artists at nooitgedacght</h3>
+        </div>
+        <div class="post-instance">
+            <h5>Dining with the artists at nooitgedacght</h3>
+        </div>
+    </div>
+</div>
--- template-parts/components/logo-reel.php
+++ template-parts/components/logo-reel.php
@@ -1,1 +1,14 @@
-<div class="logo-reel">Logo reel Section</div>
+<div class="logo-reel">
+    <div class="logo-container">
+        <!-- First set of logos -->
+        <div class="logo" style="background-image: url('https://tukuz.com/wp-content/uploads/2020/10/sanlam-logo-vector.png');"></div>
+        <div class="logo" style="background-image: url('https://culturedvultures.com/wp-content/uploads/2022/09/Dragon-Ball-Z-803x452.jpeg');"></div>
+        <div class="logo" style="background-image: url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTFzg2hjuaIXosskRnI7QszQCc7bb5M1G3w45v51K7Rg&s');"></div>
+        <div class="logo" style="background-image: url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuo_xSq8CpCKt9uGYzHQSADDpdqim94QhbH61daj6ksrjcVn8ANUl_mJL2UKHBh2XW4Gg&usqp=CAU');"></div>
+        <!-- Duplicate - Second set of logos -->
+        <div class="logo" style="background-image: url('https://tukuz.com/wp-content/uploads/2020/10/sanlam-logo-vector.png');"></div>
+        <div class="logo" style="background-image: url('https://culturedvultures.com/wp-content/uploads/2022/09/Dragon-Ball-Z-803x452.jpeg');"></div>
+        <div class="logo" style="background-image: url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTFzg2hjuaIXosskRnI7QszQCc7bb5M1G3w45v51K7Rg&s');"></div>
+        <div class="logo" style="background-image: url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuo_xSq8CpCKt9uGYzHQSADDpdqim94QhbH61daj6ksrjcVn8ANUl_mJL2UKHBh2XW4Gg&usqp=CAU');"></div>
+    </div>
+</div>
