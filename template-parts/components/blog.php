ca1e17d2cd3f1d723f1cb131a29fe0f41b0ab5e8
Andries Bester <andriesbester@Andriess-MacBook-Air.local>
2024-02-08 14:54:22 +0200

    added blurring effect to the blog roll

--- template-parts/components/blog.php
+++ template-parts/components/blog.php
@@ -1,13 +1,20 @@
-<div id="blog" class="blog">
-    <div class="instance-group">
-        <div class="post-instance">
-            <h5>Dining with the artists at nooitgedacght</h5>
-        </div>
-        <div class="post-instance">
-            <h5>Dining with the artists at nooitgedacght</h5>
-        </div>
-        <div class="post-instance">
-            <h5>Dining with the artists at nooitgedacght</h5>
-        </div>
+<div class="blog">
+  <div class="instance-group">
+    
+    <div class="post-instance" onclick="window.location='https://www.google.com';" style="cursor: pointer;">
+        <div class="background" style="background: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url('https://culturedvultures.com/wp-content/uploads/2022/09/Dragon-Ball-Z-803x452.jpeg') center / cover no-repeat; transition: filter 0.5s ease; z-index: -1;"></div>
+        <h5>Title of the wonderful article</h5>
     </div>
+
+    <div class="post-instance" onclick="window.location='https://www.google.com';" style="cursor: pointer;">
+        <div class="background" style="background: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url('https://culturedvultures.com/wp-content/uploads/2022/09/Dragon-Ball-Z-803x452.jpeg') center / cover no-repeat; transition: filter 0.5s ease; z-index: -1;"></div>
+        <h5>Title of the wonderful article</h5>
+    </div>
+
+    <div class="post-instance" onclick="window.location='https://www.google.com';" style="cursor: pointer;">
+        <div class="background" style="background: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url('https://culturedvultures.com/wp-content/uploads/2022/09/Dragon-Ball-Z-803x452.jpeg') center / cover no-repeat; transition: filter 0.5s ease; z-index: -1;"></div>
+        <h5>Title of the wonderful article</h5>
+    </div>
+
+  </div>
 </div>
