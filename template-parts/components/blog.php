ea684e251eaa5f77470807a36288600d7305f6e1
Andries Bester <andriesbester@Andriess-MacBook-Air.local>
2024-02-08 16:28:50 +0200

    made the blog roll dynamic

--- template-parts/components/blog.php
+++ template-parts/components/blog.php
@@ -1,20 +1,30 @@
 <div class="blog">
   <div class="instance-group">
-    
-    <div class="post-instance" onclick="window.location='https://www.google.com';" style="cursor: pointer;">
-        <div class="background" style="background: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url('https://culturedvultures.com/wp-content/uploads/2022/09/Dragon-Ball-Z-803x452.jpeg') center / cover no-repeat; transition: filter 0.5s ease; z-index: -1;"></div>
-        <h5>Title of the wonderful article</h5>
-    </div>
+    <?php
+    // Define the query
+    $args = array(
+        'posts_per_page' => 3, // Adjust the number of posts as needed
+        'orderby' => 'date', // Order by date
+        'order' => 'DESC' // Latest posts first
+    );
+    $query = new WP_Query($args);
 
-    <div class="post-instance" onclick="window.location='https://www.google.com';" style="cursor: pointer;">
-        <div class="background" style="background: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url('https://culturedvultures.com/wp-content/uploads/2022/09/Dragon-Ball-Z-803x452.jpeg') center / cover no-repeat; transition: filter 0.5s ease; z-index: -1;"></div>
-        <h5>Title of the wonderful article</h5>
-    </div>
+    // Loop through the posts
+    if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post();
+        // Get the post thumbnail URL
+        $thumbnail_url = get_the_post_thumbnail_url(get_the_ID(), 'full');
 
-    <div class="post-instance" onclick="window.location='https://www.google.com';" style="cursor: pointer;">
-        <div class="background" style="background: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url('https://culturedvultures.com/wp-content/uploads/2022/09/Dragon-Ball-Z-803x452.jpeg') center / cover no-repeat; transition: filter 0.5s ease; z-index: -1;"></div>
-        <h5>Title of the wonderful article</h5>
-    </div>
-
+        // Use a default image if no thumbnail is set
+        $background_image_url = $thumbnail_url ? $thumbnail_url : 'https://culturedvultures.com/wp-content/uploads/2022/09/Dragon-Ball-Z-803x452.jpeg';
+    ?>
+        <div class="post-instance" onclick="window.location='<?php the_permalink(); ?>';" style="cursor: pointer;">
+            <div class="background" style="background: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url('<?php echo esc_url($background_image_url); ?>') center / cover no-repeat; transition: filter 0.5s ease; z-index: -1;"></div>
+            <h5><?php the_title(); ?></h5>
+        </div>
+    <?php
+    endwhile; endif;
+    // Reset post data
+    wp_reset_postdata();
+    ?>
   </div>
 </div>
--- template-parts/components/services.php
+++ template-parts/components/services.php
@@ -1,44 +1,34 @@
 <div id="services" class="services">
     <div class="services-heading-block">
         <h2>Services</h2>
-        <p>Explore our comprehensive range of tailored solutions designed to meet your unique needs</p>
+        <p>Explore our comprehensive range of tailored solutions designed to meet your unique needs.</p>
     </div>
     <div class="services-card-group">
-        <div class="card" onclick="location.href='https://www.rollingstone.com/';">
-            <div class="card-top">
-                image placeholder
+        <?php
+        $categories = get_categories(array(
+            'orderby' => 'name',
+            'order'   => 'ASC'
+        ));
+        foreach ($categories as $category) {
+            // Get the link to the category archive page
+            $category_link = get_category_link($category->term_id);
+
+            // Get the ACF image field for the current category
+            $category_image = get_field('category_image', 'category_' . $category->term_id); // Make sure to prepend 'category_' to the term ID
+            
+            // Use a default image if no ACF image is set
+            $image_url = $category_image ? $category_image['url'] : 'https://i.ytimg.com/vi/AZ9NlOaS_3U/maxresdefault.jpg';
+            ?>
+            <div class="card" onclick="location.href='<?php echo esc_url($category_link); ?>';" style="cursor: pointer;">
+                <div class="card-top" style="background: url('<?php echo esc_url($image_url); ?>') center / cover no-repeat;">
+                </div>
+                <div class="card-bottom">
+                    <h5><?php echo esc_html($category->name); ?></h5>
+                    <p><?php echo esc_html($category->description); ?></p>
+                </div>
             </div>
-            <div class="card-bottom">
-                <h5>Corporate Functions</h5>
-                <p>See how we’ve helped other businesses rest easy.</p>
-            </div>
-        </div>
-        <div class="card" onclick="location.href='https://www.rollingstone.com/';">
-            <div class="card-top">
-                image placeholder
-            </div>
-            <div class="card-bottom">
-                <h5>Corporate Functions</h5>
-                <p>See how we’ve helped other businesses rest easy.</p>
-            </div>
-        </div>
-        <div class="card" onclick="location.href='https://www.rollingstone.com/';">
-            <div class="card-top">
-                image placeholder
-            </div>
-            <div class="card-bottom">
-                <h5>Corporate Functions</h5>
-                <p>See how we’ve helped other businesses rest easy.</p>
-            </div>
-        </div>
-        <div class="card" onclick="location.href='https://www.rollingstone.com/';">
-            <div class="card-top">
-                image placeholder
-            </div>
-            <div class="card-bottom">
-                <h5>Corporate Functions</h5>
-                <p>See how we’ve helped other businesses rest easy.</p>
-            </div>
-        </div>
+            <?php
+        }
+        ?>
     </div>
 </div>
