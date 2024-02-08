024cded3a8b67a27757c719d3e4aa078b8aae8e7
Andries Bester <andriesbester@Andriess-MacBook-Air.local>
2024-02-08 10:09:58 +0200

    working on some small areas

--- template-parts/components/blog.php
+++ template-parts/components/blog.php
@@ -1,13 +1,13 @@
 <div class="blog">
     <div class="instance-group">
         <div class="post-instance">
-            <h5>Dining with the artists at nooitgedacght</h3>
+            <h5>Dining with the artists at nooitgedacght</h5>
         </div>
         <div class="post-instance">
-            <h5>Dining with the artists at nooitgedacght</h3>
+            <h5>Dining with the artists at nooitgedacght</h5>
         </div>
         <div class="post-instance">
-            <h5>Dining with the artists at nooitgedacght</h3>
+            <h5>Dining with the artists at nooitgedacght</h5>
         </div>
     </div>
 </div>
