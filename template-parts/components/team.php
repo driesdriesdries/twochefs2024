3ceb4be6daee210d1034239a040efce07763fe81
Andries Bester <andriesbester@Andriess-MacBook-Air.local>
2024-02-08 17:50:49 +0200

    Team Section is now dynamic

--- template-parts/components/team.php
+++ template-parts/components/team.php
@@ -1,12 +1,19 @@
 <div id="team" class="team">
-	<div class="left">
-		<div class="content">
-			<h2>About the team</h2>
-			<p>Discover the heart of Two Chefs Catering: a blend of founders Herman Hanekom and Andre Schoeman's ambitious vision with a team of passionate, skilled professionals. Together, we ensure every detail of your event is perfected, reflecting our commitment to innovation, excellence, and exceptional catering.</p>
-		</div>
-	</div>
-	<div class="right">
-		<div class="image">
-		</div>
-	</div>
+    <div class="left">
+        <div class="content">
+            <!-- Fetch and display the ACF field for the team section heading -->
+            <h2><?php echo esc_html(get_field('team_section_heading')); ?></h2>
+            <p><?php echo esc_html(get_field('team_section_description')); ?></p>
+        </div>
+    </div>
+    <div class="right">
+        <?php 
+        $image = get_field('team_section_image');
+        if( !empty($image) ): ?>
+            <div class="image" style="background: url('<?php echo esc_url($image['url']); ?>') center / cover no-repeat;"></div>
+        <?php else: ?>
+            <!-- Optional: Default image or placeholder if no image is set -->
+            <div class="image" style="background: url('https://culturedvultures.com/wp-content/uploads/2022/09/Dragon-Ball-Z-803x452.jpeg') center / cover no-repeat;"></div>
+        <?php endif; ?>
+    </div>
 </div>
