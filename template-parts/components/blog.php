19fd82e825f024d3e00a463a16de9d23c9321098
Andries Bester <andriesbester@Andriess-Air.lan>
2024-02-10 12:36:55 +0200

    updated the image sizes being used

--- template-parts/components/blog.php
+++ template-parts/components/blog.php
@@ -11,8 +11,8 @@
 
     // Loop through the posts
     if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post();
-        // Get the post thumbnail URL
-        $thumbnail_url = get_the_post_thumbnail_url(get_the_ID(), 'full');
+        // Get the post thumbnail URL, now using 'medium_large'
+        $thumbnail_url = get_the_post_thumbnail_url(get_the_ID(), 'medium_large');
 
         // Use a default image if no thumbnail is set
         $background_image_url = $thumbnail_url ? $thumbnail_url : 'https://culturedvultures.com/wp-content/uploads/2022/09/Dragon-Ball-Z-803x452.jpeg';
--- template-parts/components/team.php
+++ template-parts/components/team.php
@@ -1,7 +1,6 @@
 <div id="team" class="team">
     <div class="left">
         <div class="content">
-            <!-- Fetch and display the ACF field for the team section heading -->
             <h2><?php echo esc_html(get_field('team_section_heading')); ?></h2>
             <p><?php echo esc_html(get_field('team_section_description')); ?></p>
         </div>
@@ -9,11 +8,13 @@
     <div class="right">
         <?php 
         $image = get_field('team_section_image');
-        if( !empty($image) ): ?>
-            <div class="image" style="background: url('<?php echo esc_url($image['url']); ?>') center / cover no-repeat;"></div>
+        if( !empty($image) ):
+            // Change 'large' to 'full' if you need the original size
+            $image_url = $image['sizes']['large'] ?? $image['url']; // Fallback to full image if 'large' size isn't available
+        ?>
+            <div class="image" style="background: url('<?php echo esc_url($image_url); ?>') center / cover no-repeat;"></div>
         <?php else: ?>
-            <!-- Optional: Default image or placeholder if no image is set -->
-            <div class="image" style="background: url('https://culturedvultures.com/wp-content/uploads/2022/09/Dragon-Ball-Z-803x452.jpeg') center / cover no-repeat;"></div>
+            <div class="image" style="background: url('default-placeholder-image-url.jpg') center / cover no-repeat;"></div>
         <?php endif; ?>
     </div>
 </div>
