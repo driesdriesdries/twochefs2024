bce3d2f82b1469a90a12acb4773447993428314b
Andries Bester <andriesbester@Andriess-MacBook-Air.local>
2024-02-12 15:36:37 +0200

    made the slider dynamic

--- template-parts/components/logo-reel.php
+++ template-parts/components/logo-reel.php
@@ -1,14 +1,35 @@
 <div class="logo-reel">
     <div class="logo-container">
-        <!-- First set of logos -->
-        <div class="logo" style="background-image: url('https://tukuz.com/wp-content/uploads/2020/10/sanlam-logo-vector.png');"></div>
-        <div class="logo" style="background-image: url('https://culturedvultures.com/wp-content/uploads/2022/09/Dragon-Ball-Z-803x452.jpeg');"></div>
-        <div class="logo" style="background-image: url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTFzg2hjuaIXosskRnI7QszQCc7bb5M1G3w45v51K7Rg&s');"></div>
-        <div class="logo" style="background-image: url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuo_xSq8CpCKt9uGYzHQSADDpdqim94QhbH61daj6ksrjcVn8ANUl_mJL2UKHBh2XW4Gg&usqp=CAU');"></div>
-        <!-- Duplicate - Second set of logos -->
-        <div class="logo" style="background-image: url('https://tukuz.com/wp-content/uploads/2020/10/sanlam-logo-vector.png');"></div>
-        <div class="logo" style="background-image: url('https://culturedvultures.com/wp-content/uploads/2022/09/Dragon-Ball-Z-803x452.jpeg');"></div>
-        <div class="logo" style="background-image: url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTFzg2hjuaIXosskRnI7QszQCc7bb5M1G3w45v51K7Rg&s');"></div>
-        <div class="logo" style="background-image: url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuo_xSq8CpCKt9uGYzHQSADDpdqim94QhbH61daj6ksrjcVn8ANUl_mJL2UKHBh2XW4Gg&usqp=CAU');"></div>
+        <?php
+        // First, let's count the number of logos
+        $total_logos = count(get_field('add_logo'));
+        // Calculate the flex-basis percentage
+        $flex_basis = $total_logos > 0 ? 100 / $total_logos : 100;
+        ?>
+
+        <?php if( have_rows('add_logo') ): ?>
+            <?php while( have_rows('add_logo') ): the_row(); ?>
+                <?php 
+                $logo_image = get_sub_field('logo_to_be_added');
+                if( !empty($logo_image) ): 
+                    $logo_url = $logo_image['url'];
+                ?>
+                    <div class="logo" style="background-image: url('<?php echo esc_url($logo_url); ?>'); flex: 0 0 <?php echo esc_attr($flex_basis); ?>%;"></div>
+                <?php endif; ?>
+            <?php endwhile; ?>
+        <?php endif; ?>
+
+        <!-- Repeat the loop if you want to duplicate the set of logos -->
+        <?php if( have_rows('add_logo') ): ?>
+            <?php while( have_rows('add_logo') ): the_row(); ?>
+                <?php 
+                $logo_image = get_sub_field('logo_to_be_added');
+                if( !empty($logo_image) ): 
+                    $logo_url = $logo_image['url'];
+                ?>
+                    <div class="logo" style="background-image: url('<?php echo esc_url($logo_url); ?>'); flex: 0 0 <?php echo esc_attr($flex_basis); ?>%;"></div>
+                <?php endif; ?>
+            <?php endwhile; ?>
+        <?php endif; ?>
     </div>
 </div>
