880e68e518b75f2e6ad91e855fd40c64975f5ea4
Andries Bester <andriesbester@Andriess-MacBook-Air.local>
2024-02-13 10:54:20 +0200

    recreated the logo reel

--- template-parts/components/logo-reel.php
+++ template-parts/components/logo-reel.php
@@ -1,3 +1,46 @@
+<style>
+.logo-reel {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  align-items: center;
+  background-color: white;
+}
+
+.logo {
+  flex: 1 0 calc(16.666% - 10px);
+  margin: 5px;
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
+  opacity: 0;
+  animation: fadeInLogo 0.5s ease forwards;
+}
+
+@keyframes fadeInLogo {
+  to {
+    opacity: 1;
+  }
+}
+
+<?php if( have_rows('add_logo') ): ?>
+  <?php $i = 0; ?>
+  <?php while( have_rows('add_logo') ): the_row(); $i++; ?>
+    .logo:nth-child(<?php echo $i; ?>) {
+      animation-delay: <?php echo $i * (2.5 / count(get_field('add_logo'))); ?>s;
+    }
+  <?php endwhile; ?>
+<?php endif; ?>
+</style>
+
 <div class="logo-reel">
-    <h1>TESTING</h1>
+  <?php if( have_rows('add_logo') ): ?>
+    <?php while( have_rows('add_logo') ): the_row(); 
+      $logo_image = get_sub_field('logo_to_be_added');
+      $logo_url = esc_url($logo_image['url']);
+    ?>
+      <div class="logo" style="background-image: url('<?php echo $logo_url; ?>'); min-width: 150px; max-width: calc(16.666% - 10px); height: 6rem;">
+      </div>
+    <?php endwhile; ?>
+  <?php endif; ?>
 </div>
