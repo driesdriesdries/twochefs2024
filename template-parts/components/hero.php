458e024345234ac3e2d1d52f9dc183b0de28626b
Andries Bester <andriesbester@Andriess-MacBook-Air.local>
2024-02-13 12:07:14 +0200

    created hero

--- template-parts/components/hero.php
+++ template-parts/components/hero.php
@@ -1,1 +1,17 @@
-<div class="hero">Hero Section</div>
+<?php
+// Variables for the ACF fields
+$hero_image = get_field('hero_section_image');
+$hero_heading = get_field('hero_section_heading');
+$hero_description = get_field('hero_section_description');
+
+// Get the URL of the 'large' image size or a custom registered size
+$hero_image_url = $hero_image['sizes']['large']; // Replace 'large' with your custom size if needed
+?>
+
+<div class="hero" style="background-image: url('<?php echo esc_url($hero_image_url); ?>');">
+  <div class="hero-content">
+    <h1><?php echo esc_html($hero_heading); ?></h1>
+    <p><?php echo esc_html($hero_description); ?></p>
+    <a href="#" class="button button-primary">Contact</a>
+  </div>
+</div>
